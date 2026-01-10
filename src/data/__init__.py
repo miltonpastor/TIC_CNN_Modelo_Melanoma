da11d17b8f03e5e42a75faa2739bd8c4ec380488
Milton <milton.a.pastor.f@gmail.com>
2026-01-10 17:34:51 -0500

    corregir: error init

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,19 +1,18 @@
-from .data_loader import load_and_clean_data
+from .data_loader import load_and_clean_data, load_predivided_data
 from .split_data import create_splits
 from .preprocessing import (
-    create_data_generators,
-    create_data_flow_from_dataframe,
-    load_and_preprocess_image,
-    # remove_hair_artifacts,
-    # preprocess_with_cleaning
+    create_tf_dataset,
+    get_augmentation_layers,
+    oversample_minority_class,
+    load_and_preprocess_image_tf
 )
 
 __all__ = [
-    'load_and_clean_data', 
+    'load_and_clean_data',
+    'load_predivided_data',
     'create_splits',
-    'create_data_generators',
-    'create_data_flow_from_dataframe',
-    'load_and_preprocess_image',
-    # 'remove_hair_artifacts',
-    # 'preprocess_with_cleaning'
+    'create_tf_dataset',
+    'get_augmentation_layers',
+    'oversample_minority_class',
+    'load_and_preprocess_image_tf'
 ]
